b8db498a0f6c7d17fc9b7f01a42f8441a83de616
dev <dev@A21PC15>
2025-02-24 17:51:39 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	 cout << "Every\n      hunter\n\t     wants \n\t\t   to \n\t\t      know\n\t\t\t   where\n\t\t\t\t the \n\t\t\t             pheasant\n\t\t\t\t\t      is\n\t\t\t\t\t         sitting.";
+
+	cout <<" You tricked me (folk song)\n\t           Verse 1:\n\t\t\t   You tricked me\n\t\t\t   And I spent it on the river,\n\t\t\t   You tricked me\n\t\t\t   And I spent it on the river.";
+
+
+
+
+	cout << "For Sale: Lifan City-R200 Motorcycle!\nModel: Lifan City-R200\nYear: 2020\nMileage: 15,000 km\nCondition: Excellent, ready to ride\nKey Features:\nEngine capacity: 200 cc\nEngine type: 4-stroke, single-cylinder\nManual transmissionn\nLocation: Rivne, Kustyn\nPrice: 35,000 UAH\nContact:+380*********\n+380     | *** |\n**       | **  |";
+
+}
